215df9b0d19eb039a863f0bd4aff1af65e9b27ec
Oasis Jeffries <oasis.jeffries@gmail.com>
2022-06-18 17:28:35 -0400

    Created way to add new post, changed navigation bar, and added page titles

--- theblog/models.py
+++ theblog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -10,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        #  return reverse('article-detail', args=(str(self.id)))
+        return reverse('home')
