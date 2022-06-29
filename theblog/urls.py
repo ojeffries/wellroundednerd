eea1062dc01a65c47e8cf527a3781f6157264ea0
Oasis Jeffries <oasis.jeffries@gmail.com>
2022-06-29 15:55:30 -0400

    Post Updates and Edits

--- theblog/urls.py
+++ theblog/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',HomeView.as_view() , name= "home"),
+    path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
-
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
 ]
--- theblog/views.py
+++ theblog/views.py
@@ -20,3 +20,9 @@ class AddPostView(CreateView):
     model = Post
     template_name = 'add_post.html'
     fields = '__all__'
+
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'update_post.html'
+    fields = ['title', 'body']
